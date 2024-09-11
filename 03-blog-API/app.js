44f012f557c7b2372d9f9e31d28b4c9ab9d0149f
Carlos Alonso <cbm.alonso@gmail.com>
2024-09-11 19:51:09 -0400

    Simple fetch and render posts from the server.

--- 03-blog-API/app.js
+++ 03-blog-API/app.js
@@ -1,3 +1,50 @@
 
-const URL = 'https://jsonplaceholder.typicode.com/posts';
-console.log('Hello World!', URL);
+const urlBase = 'https://jsonplaceholder.typicode.com/';
+const postSlug = 'posts';
+const userSlug =  'users/1/albums'
+
+const URL_POSTS = `${urlBase}${postSlug}`;
+// const URL_USERS = `${urlBase}${userSlug}`;
+
+// Fetching data from the server
+const fetchPosts = async () => {
+    try {
+        const response = await fetch(URL_POSTS);
+        const data = await response.json();
+        renderPosts(data);
+    }
+    catch (error) {
+        console.log('Error: ', error);
+    }
+}
+// Add event listener 
+window.addEventListener('load', () => {
+    fetchPosts();
+});
+
+// fetch(URL_USERS)
+//   .then((response) => response.json())
+//   .then((users) => console.log(users));
+
+function renderPosts(posts) {
+    posts.forEach(post => {
+        const { id, title, body } = post;
+        const listadoPosts = document.getElementById('listado-posts');
+
+        const postLi = document.createElement('li');
+        postLi.id = `id-${id}`;
+        postLi.classList.add('border-l-2', 'pl-4','mb-4', 'border-cyan-900' );
+
+        const postTitle = document.createElement('h3');
+        postTitle.classList.add('text-xl', 'font-semibold', 'capitalize');
+        postTitle.textContent = title;
+
+        const postBody = document.createElement('p');
+        postBody.textContent = body;
+
+        postLi.appendChild(postTitle);
+        postLi.appendChild(postBody);
+        listadoPosts.appendChild(postLi);
+
+    });
+}
