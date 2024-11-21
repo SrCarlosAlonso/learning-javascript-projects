3d7fbe6f37c0464749d3f76a4774f7ec08ac4c2f
Carlos Alonso <cbm.alonso@gmail.com>
2024-11-20 22:34:15 -0500

    Refactor searchs and start funcionality for filters

--- 09-image-search/main.js
+++ 09-image-search/main.js
@@ -1,17 +1,19 @@
 // Variables de entorno
 const AKEY_UNSPLASH = import.meta.env.VITE_UNSPLASH_AKEY;
 const SKEY_UNSPLASH = import.meta.env.VITE_UNSPLASH_SKEY;
-const AKEY_PIXABAY = import.meta.env.VITE_PIXABAY_AKEY
+const AKEY_PIXABAY = import.meta.env.VITE_PIXABAY_KEY
 
 
 let source = 'yellow+flowers';
-const URL_PIXAVAY = `https://pixabay.com/api/?key=8305684-c57a424dfd0b3763951a9869a&q=${source}`;
-let URL = URL_PIXAVAY;
+const URL_PIXABAY = `https://pixabay.com/api/?key=${AKEY_PIXABAY}&q=`;
+let URL = URL_PIXABAY + source;
 
 // DOM
 const imgContainer = document.querySelector('#images-container');
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
+const filter_pixabay = document.querySelector('#filter-pixabay');
+const filter_unspash = document.querySelector('#filter-unspash');
 
 let backgroundHeader = 'https://images.unsplash.com/photo-1493673272479-a20888bcee10'
 
@@ -36,7 +38,7 @@ async function renderImage(obj) {
   while (imgContainer.firstChild) {
     imgContainer.removeChild(imgContainer.firstChild);
   }
-  // Render images
+
   obj.forEach(image => {
     const img = document.createElement('img');
     img.src = image.webformatURL;
@@ -45,25 +47,43 @@ async function renderImage(obj) {
     imgContainer.appendChild(img);
   });
 }
-
-// Eventos
+/*
+ * Definir origen de imagenes
+ */
 document.addEventListener('DOMContentLoaded', function () {
-  changeBackground(backgroundHeader);
+  filter_pixabay.addEventListener('click', (e) => {
+    const target = e.target;
+    console.log(target);
+  });
 });
-
+/*
+ * Eventos de carga de imagenes
+ */
+// Load background image at start
+document.addEventListener('DOMContentLoaded', function () {
+  changeBackground();
+});
+// Change background image
 function changeBackground(url) {
-  backgroundHeader = url;
+  backgroundHeader = url || backgroundHeader;
   document.documentElement.style.setProperty('--background-header', `url(${backgroundHeader})`);
 }
 
-
-// Eventos
+// Eventos del formulario
 searchInput.addEventListener('change', (e) => {
   e.preventDefault();
   source = e.target.value;
 });
 
+// Observamos el evento submit para regenerar imagenes
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(source);
+  source = searchInput.value;
+  if (source === '') {
+    alert('Debes escribir algo para buscar')
+    return;
+  }
+  URL = URL_PIXABAY + source;
+  getData();
 });
+
