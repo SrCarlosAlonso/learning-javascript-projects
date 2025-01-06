a4f2d85926c69630de3c96099856811bd9884d79
Carlos Alonso <cbm.alonso@gmail.com>
2025-01-05 20:14:06 -0500

    Refactor HTML structure and update main.js to use a constant for the title

--- 10-http-app/src/main.js
+++ 10-http-app/src/main.js
@@ -1,15 +1,18 @@
 import "./style.css";
 
-// API:
+// API
 // https://rickandmortyapi.com/documentation/
 // https://breakingbadquotes.xyz/
 
+const title = "Hello Vite!";
+
 document.querySelector("#app").innerHTML = `
   <div>
-    <h1 id="app-title"> Hello Vite!</h1>
+    <h1 id="app-title"> ${title} </h1>
     <div class="card">
     </div>
   </div>
+
 `;
 
 setupCounter(document.querySelector("#counter"));
