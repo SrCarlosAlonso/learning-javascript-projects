40f4f47066906d816249d640c3877fe747335c41
Carlos Alonso <cbm.alonso@gmail.com>
2025-01-07 21:31:26 -0500

    Implement pagination in character display; refactor getCharacters function to accept URL parameter

--- 10-http-app/src/main.js
+++ 10-http-app/src/main.js
@@ -13,10 +13,10 @@ appHeading.appendChild(appTitle);
 
 const urlCharacters = "https://rickandmortyapi.com/api/character";
 
-const getCharacters = async () => {
+const getCharacters = async (url) => {
 
   try {
-    const response = await fetch(urlCharacters);
+    const response = await fetch(url);
     const data = await response.json();
     const characters = data.results;
     const pagination = data.info;
@@ -32,7 +32,7 @@ const getCharacters = async () => {
   }
 }
 
-getCharacters();
+getCharacters(urlCharacters);
 
 const printCard = (objCharacter) => {
   const { name, image } = objCharacter;
@@ -54,6 +54,53 @@ const printCard = (objCharacter) => {
 
 const printPagination = (info) => {
   const { pages, next, prev } = info;
+  const pagination = document.createElement("ul");
+  pagination.classList.add("pagination");
+
+  const prevButton = document.createElement("li");
+  prevButton.innerText = "← Prev";
+  prevButton.id = "pag-prev";
+  prevButton.addEventListener("click", () => {
+    if (prev === null) return;
+    deleteChildren(charactersWrapper);
+    deleteChildren(pagination);
+    getCharacters(prev);
+  });
+  pagination.appendChild(prevButton);
+
+  let pagesMin = 1;
+  let pagesMax = 3;
+  for (let i = pagesMin; i <= pagesMax; i++) {
+    const number = document.createElement("li");
+    number.innerText = i;
+    number.addEventListener("click", () => {
+      deleteChildren(charactersWrapper);
+      deleteChildren(pagination);
+      getCharacters(`${urlCharacters}?page=${i}`);
+    });
+    pagination.appendChild(number);
+  }
+
+  // Hacer que no se vean todos los números
+  // Hacer que se vean los números de la página actual y las dos anteriores y las dos siguientes
+
+  const nextButton = document.createElement("li");
+  nextButton.innerText = "Next →";
+  nextButton.id = "pag-next";
+  nextButton.addEventListener("click", () => {
+    deleteChildren(charactersWrapper);
+    deleteChildren(pagination);
+    getCharacters(next);
+  });
+  pagination.appendChild(nextButton);
+
+  appFooter.appendChild(pagination);
+
+  console.log(
+    `Pages: ${pages},
+    Next: ${next},
+    Prev: ${prev}`
+  )
 }
 
 
